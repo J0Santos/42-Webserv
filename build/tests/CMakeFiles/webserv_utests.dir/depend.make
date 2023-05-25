ddd3185fe518879cd70fa859367419eb7b721bfb
Maria Sottomayor <79856624+mlanca-c@users.noreply.github.com>
2023-05-25 10:36:53 +0100

    Delete build directory

--- build/tests/CMakeFiles/webserv_utests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for webserv_utests.
-# This may be replaced when dependencies are built.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mlancac/.Cursus/lv5/my-webserv/tests
-# Build directory: /home/mlancac/.Cursus/lv5/my-webserv/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(webserv_gtest "webserv_utests")
-set_tests_properties(webserv_gtest PROPERTIES  _BACKTRACE_TRIPLES "/home/mlancac/.Cursus/lv5/my-webserv/tests/CMakeLists.txt;27;add_test;/home/mlancac/.Cursus/lv5/my-webserv/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
