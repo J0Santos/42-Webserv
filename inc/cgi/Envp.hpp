170490eadeebc0cfdf01038ce4c89bd09f21e73b
mlanca-c <marialsottomayor@gmail.com>
2023-05-29 22:24:18 +0100

    started envp

---
+++ inc/cgi/Envp.hpp
@@ -0,0 +1,35 @@
+#ifndef CGI_ENVP_HPP
+#define CGI_ENVP_HPP
+
+#include "http/Request.hpp"
+#include "utils/Route.hpp"
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+namespace cgi {
+class Envp {
+    public:
+
+        Envp(http::Request const& request, http::Route const& route);
+        ~Envp(void);
+
+        void fillEnvp(std::string name, std::string val);
+        std::map<std::string, std::string> getMapEnvp(void) const;
+
+        std::string get(std::string const& key) const;
+
+    private:
+
+        std::vector<std::string>
+            splitInfoFromPath(std::string const& path) const;
+
+        std::map<std::string, std::string> m_envp;
+};
+
+} // namespace cgi
+
+std::ostream& operator<<(std::ostream& os, cgi::Envp const& ctx);
+
+#endif /* CGI_ENVP_HPP */
