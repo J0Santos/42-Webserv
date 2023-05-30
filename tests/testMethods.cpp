6c9469cf96fd209d930a880e4edfac3e6c8851f3
mlanca-c <marialsottomayor@gmail.com>
2023-05-30 13:12:32 +0100

    changed Method enum type name to MethodType

--- tests/testMethods.cpp
+++ tests/testMethods.cpp
@@ -3,17 +3,17 @@
 #include <gtest/gtest.h>
 
 TEST(testConvert, testConvertMethod) {
-    ASSERT_EQ(http::convertMethod("GET"), http::Method::GET);
-    ASSERT_EQ(http::convertMethod("HEAD"), http::Method::HEAD);
-    ASSERT_EQ(http::convertMethod("POST"), http::Method::POST);
-    ASSERT_EQ(http::convertMethod("PUT"), http::Method::PUT);
-    ASSERT_EQ(http::convertMethod("DELETE"), http::Method::DELETE);
-    ASSERT_EQ(http::convertMethod("CONNECT"), http::Method::CONNECT);
-    ASSERT_EQ(http::convertMethod("OPTIONS"), http::Method::OPTIONS);
-    ASSERT_EQ(http::convertMethod("TRACE"), http::Method::TRACE);
-    ASSERT_EQ(http::convertMethod("PATCH"), http::Method::PATCH);
-    ASSERT_EQ(http::convertMethod("INVALID"), http::Method::INVALID);
-    ASSERT_EQ(http::convertMethod("other"), http::Method::UNKNOWN_METHOD);
+    ASSERT_EQ(http::convertMethod("GET"), http::MethodType::GET);
+    ASSERT_EQ(http::convertMethod("HEAD"), http::MethodType::HEAD);
+    ASSERT_EQ(http::convertMethod("POST"), http::MethodType::POST);
+    ASSERT_EQ(http::convertMethod("PUT"), http::MethodType::PUT);
+    ASSERT_EQ(http::convertMethod("DELETE"), http::MethodType::DELETE);
+    ASSERT_EQ(http::convertMethod("CONNECT"), http::MethodType::CONNECT);
+    ASSERT_EQ(http::convertMethod("OPTIONS"), http::MethodType::OPTIONS);
+    ASSERT_EQ(http::convertMethod("TRACE"), http::MethodType::TRACE);
+    ASSERT_EQ(http::convertMethod("PATCH"), http::MethodType::PATCH);
+    ASSERT_EQ(http::convertMethod("INVALID"), http::MethodType::INVALID);
+    ASSERT_EQ(http::convertMethod("other"), http::MethodType::UNKNOWN_METHOD);
 }
 
 TEST(testConvert, testConvertVersion) {
