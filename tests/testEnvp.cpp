c1466dbb4740bbe3cc2eb4a46fc9dc9c46af5a0c
mlanca-c <marialsottomayor@gmail.com>
2023-06-08 13:00:58 +0100

    fixed envp tests

--- tests/testEnvp.cpp
+++ tests/testEnvp.cpp
@@ -12,14 +12,20 @@ TEST(testEnvp, testBasic) {
                           "Accept-Encoding: gzip, deflate, br\r\n"
                           "Accept-Language: en-US,en;q=0.9\r\n"
                           "Connection: keep-alive\r\n\r\n"));
+    http::Route route("/cgi/", "./websites/cgi-bin/");
+    req->setRoute(route);
+
     cgi::Envp envp(req);
     char**    c_envp = envp;
     EXPECT_STREQ(c_envp[0], "REQUEST_METHOD=POST");
-    EXPECT_STREQ(c_envp[1], "SCRIPT_NAME=/cgi/myscript.py");
-    EXPECT_TRUE(!c_envp[2]);
-    // delete[] c_envp;
-    for (int i = 0; c_envp[i]; ++i) { delete[] c_envp[i]; }
-    delete[] c_envp;
+    EXPECT_STREQ(c_envp[1], "PATH_INFO=myscript.py");
+    EXPECT_STREQ(c_envp[2], "DOCUMENT_ROOT=./websites/cgi-bin/");
+    EXPECT_STREQ(c_envp[3], "QUERY_STRING=");
+    EXPECT_STREQ(c_envp[4], "CONTENT_LENGTH=");
+    EXPECT_STREQ(c_envp[5], "CONTENT_TYPE=");
+    EXPECT_TRUE(!c_envp[6]);
+
+    ft::array::erase(c_envp);
 }
 
 TEST(testEnvp, testMoreComplete) {
@@ -33,14 +39,18 @@ TEST(testEnvp, testMoreComplete) {
                           "Accept-Encoding: gzip, deflate, br\r\n"
                           "Accept-Language: en-US,en;q=0.9\r\n"
                           "Connection: keep-alive\r\n\r\n"));
+    http::Route route("/cgi/", "./websites/cgi-bin/");
+    req->setRoute(route);
+
     cgi::Envp envp(req);
     char**    c_envp = envp;
     EXPECT_STREQ(c_envp[0], "REQUEST_METHOD=GET");
-    EXPECT_STREQ(c_envp[1], "SCRIPT_NAME=/cgi/myscript.py");
-    EXPECT_STREQ(c_envp[2], "QUERY_STRING=param1=value1&param2=value2");
-    EXPECT_STREQ(c_envp[3], "CONTENT_LENGTH=0");
-    EXPECT_STREQ(c_envp[4], "CONTENT_TYPE=text/html");
-    EXPECT_TRUE(!c_envp[5]);
-    for (int i = 0; c_envp[i]; ++i) { delete[] c_envp[i]; }
-    delete[] c_envp;
+    EXPECT_STREQ(c_envp[1], "PATH_INFO=myscript.py");
+    EXPECT_STREQ(c_envp[2], "DOCUMENT_ROOT=./websites/cgi-bin/");
+    EXPECT_STREQ(c_envp[3], "QUERY_STRING=param1=value1&param2=value2");
+    EXPECT_STREQ(c_envp[4], "CONTENT_LENGTH=0");
+    EXPECT_STREQ(c_envp[5], "CONTENT_TYPE=text/html");
+    EXPECT_TRUE(!c_envp[6]);
+
+    ft::array::erase(c_envp);
 }
